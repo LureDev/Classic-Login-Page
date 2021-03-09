a71eb56a0168509872c79ee6d2ead4ee5388f89a
Timóteo <75149027+LureDev@users.noreply.github.com>
2021-03-09 20:45:22 -0300

    Added JavaScript code.
    It makes the "show password" function works, and i also made a little responsive adjusts, all this using JQuery.

---
+++ Classic Login Page/js/script.js
@@ -0,0 +1,41 @@
+$(window).on('load', () => {
+
+    var showPwdIcon = $('#show-pwd');
+    var pwdInput = $('#pwd-input');
+    var userInput = $('#username-input');
+    var isText;
+
+    showPwdIcon.click(() => {
+        isText = !isText;
+
+        if(!isText) {
+            pwdInput.attr('type', 'password')
+            showPwdIcon.attr('class', 'fas fa-eye');
+        }
+        else {
+            pwdInput.attr('type', 'text');
+            showPwdIcon.attr('class', 'fas fa-eye-slash');
+        }
+    });
+
+    responsiveOnFocus = () => {
+        let width = $(window).width(); // Gets the screen width
+        
+        if(width <= 760){  // Checking if the user is using a mobile device
+            userInput.focusin(() => {
+                $('main').css('height', 'auto').css('min-height', 'auto');
+            }).focusout(() => {
+                $('main').css('height', '100%').css('min-height', '100%');
+            });
+// Ver oq o focusin e o focusout fazem na api do jquery
+            pwdInput.focusin(() => {
+                $('main').css('height', 'auto').css('min-height', 'auto');
+            }).focusout(() => {
+                $('main').css('height', '100%').css('min-height', '100%');
+            });
+        }
+    };
+
+    responsiveOnFocus();
+
+});
